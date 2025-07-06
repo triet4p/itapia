17419ad2f9b7522994f805d583d915e3d37102d8
Le Minh Triet <119602471+trietp1253201581@users.noreply.github.com>
2025-07-06 17:02:23 +0700

    Modify PYTHONPATH

--- api_service/app/__init__.py
+++ api_service/app/__init__.py
@@ -1,9 +1,2 @@
 from . import api, core, crud, db, schemas
 __all__ = ['api', 'core', 'crud', 'db', 'schemas']
-
-
-import sys
-import os
-init_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-sys.path.append(init_dir)
