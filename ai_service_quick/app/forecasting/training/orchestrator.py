46dd294b05e52111d8fdfe9d754e6437d830c7f8
Le Minh Triet <trietlm0306@gmail.com>
2025-07-17 15:30:16 +0700

    Modify function split in orchestrator

--- ai_service_quick/app/forecasting/training/orchestrator.py
+++ ai_service_quick/app/forecasting/training/orchestrator.py
@@ -9,7 +9,7 @@ from typing import List, Dict
 
 class TrainingOrchestrator:
     def __init__(self, df: pd.DataFrame):
-        self.df = df
+        self.df = df.copy()
         self.tasks: Dict[str, _MLTask] = {}
         self.feature_cols = [c for c in df.columns if not c.startswith('target_')]
         self.feature_cols.remove('ticker')
@@ -17,19 +17,23 @@ class TrainingOrchestrator:
     def register_task(self, new_task: _MLTask):
         info(f'Training Orchestrator: Registering problem: {new_task.task_id} ({new_task.task_type})')
         self.tasks[new_task.task_id] = new_task
-        
-    def _prepare_feature_selection_each_task(self, task: _MLTask,
-                                         weights: Dict,
-                                         bonus_features: List[str],
-                                         bonus_multiplier: float):
+    
+    def _create_target_each_task(self, task: _MLTask):
         info(f'Training Orchestrator: Create target for task {task.task_id}')
         
         targets = task.create_targets(self.df, base_price_col='close')
-        target_for_fs = targets[task.target_for_selection]
+        self.df = pd.concat([self.df, targets], axis=1)
         
-        valid_indices = target_for_fs.dropna().index
-        X_fs = self.df.loc[valid_indices, self.feature_cols]
-        y_fs = target_for_fs.loc[valid_indices]
+    def _run_feature_selection_each_task(self, task: _MLTask,
+                                         weights: Dict,
+                                         bonus_features: List[str],
+                                         bonus_multiplier: float):
+        info(f'Training Orchestrator: Selection features for {task.task_id}')
+        target_col = task.target_for_selection
+        temp_df = self.df.dropna(subset=[target_col])
+        temp_df.drop(columns='ticker', inplace=True)
+        X_fs = temp_df[self.feature_cols]
+        y_fs = temp_df[target_col]
         
         info(f'Training Orchestrator: Ranking features for {task.task_id}')
         ranks_df = get_ensemble_feature_ranks(X_fs, y_fs, task_type=task.task_type)
@@ -46,7 +50,8 @@ class TrainingOrchestrator:
                               bonus_multiplier: float = 1.0):
         info(f'Training Orchestrator: Running feature selection for all tasks ...')
         for task_id, task in self.tasks.items():
-            self._prepare_feature_selection_each_task(task,
+            self._create_target_each_task(task)
+            self._run_feature_selection_each_task(task,
                                                   weights,
                                                   bonus_features, 
                                                   bonus_multiplier)
