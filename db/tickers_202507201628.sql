cffc7c51c9c08eb07105dd66b56bb6f7095c9e45
Le Minh Triet <trietlm0306@gmail.com>
2025-07-20 16:43:14 +0700

    Add some static table data scripts

---
+++ db/tickers_202507201628.sql
@@ -0,0 +1,102 @@
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('NVDA','NVIDIA Corporation','NasdaqGS','TECH',true),
+	 ('AAPL','Apple Inc.','NasdaqGS','TECH',true),
+	 ('MSFT','Microsoft Coperation','NasdaqGS','TECH',true),
+	 ('AVGO','Broadcom Inc.','NasdaqGS','TECH',true),
+	 ('TSM','Taiwan Semiconductor Manufacturing Company Limited','NYSE','TECH',true),
+	 ('QCOM','QUALCOMM Incorporated','NasdaqGS','TECH',true),
+	 ('ACN','Accenture plc','NYSE','TECH',true),
+	 ('ORCL','Oracle Operation','NYSE','TECH',true),
+	 ('ADBE','Adobe Inc.','NasdaqGS','TECH',true),
+	 ('CRM','Salesforce, Inc.','NYSE','TECH',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('AMD','Advanced Micro Devices, Inc.','NasdaqGS','TECH',true),
+	 ('INTC','Intel Corporation','NasdaqGS','TECH',true),
+	 ('JPM','JPMorgan Chase & Co.','NYSE','FIN',true),
+	 ('BAC','Bank of America Corporation','NYSE','FIN',true),
+	 ('GS','The Goldman Sachs Group, Inc.','NYSE','FIN',true),
+	 ('C','Citigroup Inc.','NYSE','FIN',true),
+	 ('MS','Morgan Stanley (MS)','NYSE','FIN',true),
+	 ('BLK','BlackRock, Inc.','NYSE','FIN',true),
+	 ('AXP','American Express Company','NYSE','FIN',true),
+	 ('V','Visa Inc.','NYSE','FIN',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('MA','Mastercard Incorporated','NYSE','FIN',true),
+	 ('WFC','Wells Fargo & Company','NYSE','FIN',true),
+	 ('BRK-B','Berkshire Hathaway Inc.','NYSE','FIN',true),
+	 ('JNJ','Johnson & Johnson','NYSE','HEALTH',true),
+	 ('PFE','Pfizer Inc.','NYSE','HEALTH',true),
+	 ('MRK','Merck & Co., Inc.','NYSE','HEALTH',true),
+	 ('ABT','Abbott Laboratories','NYSE','HEALTH',true),
+	 ('BMY','Bristol-Myers Squibb Company','NYSE','HEALTH',true),
+	 ('GILD','Gilead Sciences, Inc.','NasdaqGS','HEALTH',true),
+	 ('UNH','UnitedHealth Group Incorporated','NYSE','HEALTH',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('LLY','Eli Lilly and Company','NYSE','HEALTH',true),
+	 ('ABBV','AbbVie Inc.','NYSE','HEALTH',true),
+	 ('MDT','Medtronic plc','NYSE','HEALTH',true),
+	 ('AMZN','Amazon.com, Inc.','NasdaqGS','CODIS',true),
+	 ('TSLA','Tesla, Inc.','NasdaqGS','CODIS',true),
+	 ('HD','The Home Depot, Inc.','NYSE','CODIS',true),
+	 ('MCD','McDonald''s Corporation','NYSE','CODIS',true),
+	 ('SBUX','Starbucks Corporation','NasdaqGS','CODIS',true),
+	 ('F','Ford Motor Company','NYSE','CODIS',true),
+	 ('GM','General Motors Company','NYSE','CODIS',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('NKE','NIKE, Inc.','NYSE','CODIS',true),
+	 ('LOW','Lowe''s Companies, Inc.','NYSE','CODIS',true),
+	 ('BKNG','Booking Holdings Inc.','NasdaqGS','CODIS',true),
+	 ('PG','The Procter & Gamble Company','NYSE','COSTA',true),
+	 ('KO','The Coca-Cola Company','NYSE','COSTA',true),
+	 ('WMT','Walmart Inc.','NYSE','COSTA',true),
+	 ('CL','Colgate-Palmolive Company','NYSE','COSTA',true),
+	 ('KHC','The Kraft Heinz Company','NYSE','COSTA',true),
+	 ('COST','Costco Wholesale Corporation','NasdaqGS','COSTA',true),
+	 ('MO','Altria Group, Inc.','NYSE','COSTA',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('PEP','PepsiCo, Inc.','NasdaqGS','COSTA',true),
+	 ('MDLZ','Mondelez International, Inc.','NasdaqGS','COSTA',true),
+	 ('TGT','Target Corporation','NYSE','COSTA',true),
+	 ('XOM','Exxon Mobil Corporation','NYSE','ENER',true),
+	 ('CVX','Chevron Corporation','NYSE','ENER',true),
+	 ('COP','ConocoPhillips','NYSE','ENER',true),
+	 ('EOG','EOG Resources, Inc.','NYSE','ENER',true),
+	 ('MPC','Marathon Petroleum Corporation','NYSE','ENER',true),
+	 ('PSX','Phillips 66','NYSE','ENER',true),
+	 ('SLB','Schlumberger Limited','NYSE','ENER',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('BA','The Boeing Company','NYSE','INDUS',true),
+	 ('CAT','Caterpillar Inc.','NYSE','INDUS',true),
+	 ('GE','GE Aerospace','NYSE','INDUS',true),
+	 ('HON','Honeywell International Inc.','NasdaqGS','INDUS',true),
+	 ('LMT','Lockheed Martin Corporation','NYSE','INDUS',true),
+	 ('UPS','United Parcel Service, Inc.','NYSE','INDUS',true),
+	 ('RTX','RTX Corporation','NYSE','INDUS',true),
+	 ('DE','Deere & Company','NYSE','INDUS',true),
+	 ('NEE','NextEra Energy, Inc.','NYSE','UTIL',true),
+	 ('DUK','Duke Energy Corporation','NYSE','UTIL',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('AEP','American Electric Power Company, Inc.','NasdaqGS','UTIL',true),
+	 ('EXC','Exelon Corporation','NasdaqGS','UTIL',true),
+	 ('SO','The Southern Company','NYSE','UTIL',true),
+	 ('SRE','Sempra','NYSE','UTIL',true),
+	 ('GOOGL','Alphabet Inc.','NasdaqGS','COMM',true),
+	 ('VZ','Verizon Communications Inc.','NYSE','COMM',true),
+	 ('T','AT&T Inc.','NYSE','COMM',true),
+	 ('META','Meta Platforms, Inc.','NasdaqGS','COMM',true),
+	 ('NFLX','Netflix, Inc.','NasdaqGS','COMM',true),
+	 ('DIS','The Walt Disney Company','NYSE','COMM',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('TMUS','T-Mobile US, Inc.','NasdaqGS','COMM',true),
+	 ('CMCSA','Comcast Corporation','NasdaqGS','COMM',true),
+	 ('FCX','Freeport-McMoRan Inc.','NYSE','MATE',true),
+	 ('NEM','Newmont Corporation','NYSE','MATE',true),
+	 ('LIN','Linde plc','NasdaqGS','MATE',true),
+	 ('DOW','Dow Inc.','NYSE','MATE',true),
+	 ('APD','Air Products and Chemicals, Inc.','NYSE','MATE',true),
+	 ('PLD','Prologis, Inc.','NYSE','REES',true),
+	 ('AMT','American Tower Corporation','NYSE','REES',true),
+	 ('SPG','Simon Property Group, Inc.','NYSE','REES',true);
+INSERT INTO public.tickers (ticker_sym,company_name,exchange_code,sector_code,is_active) VALUES
+	 ('EQIX','Equinix, Inc.','NasdaqGS','REES',true),
+	 ('O','Realty Income Corporation','NYSE','REES',true);
