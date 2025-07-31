1cb2df94eaa325dd21ffca37751a3bd4f8822288
Le Minh Triet <trietlm0306@gmail.com>
2025-07-31 13:12:24 +0700

    Add data seeds

---
+++ data_seeds/data/exchanges_202507201627.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.exchanges (exchange_code,exchange_name,currency,timezone,country,open_time,close_time) VALUES
+	 ('NasdaqGS','Nasdaq Stock Market','USD','America/New_York','USA','09:15:00','16:30:00'),
+	 ('NYSE','New York Stock Exchange','USD','America/New_York','USA','09:15:00','16:30:00');
---
+++ data_seeds/scripts/seed_rules.py
@@ -0,0 +1,33 @@
+from itapia_common.rules.builtin import builtin_decision_rules, builtin_risk_rules, builtin_opportunity_rules
+from itapia_common.rules.rule import Rule
+from itapia_common.dblib.services.rules import RuleService
+from itapia_common.dblib.session import get_rdbms_session
+from itapia_common.schemas.entities.rules import RuleEntity
+from itapia_common.logger import ITAPIALogger
+from typing import List
+
+logger = ITAPIALogger('Rules Seeding')
+
+def seed_rules(rule_service: RuleService, rules: List[Rule]):
+    for rule in rules:
+        rule_dict = rule.to_dict()
+        rule_entity = RuleEntity(
+            rule_definition=rule_dict['root'],
+            **rule_dict
+        )
+        rule_service.save_rule(rule_entity)
+def seed_all():
+    rdbms_session = next(get_rdbms_session())
+    rule_service = RuleService(rdbms_session)
+    
+    logger.info('Seeding built in decision rules')
+    seed_rules(rule_service, builtin_decision_rules())
+    
+    logger.info('Seeding built in risk rules')
+    seed_rules(rule_service, builtin_risk_rules())
+    
+    logger.info('Seeding built in opportunity rules')
+    seed_rules(rule_service, builtin_opportunity_rules())
+    
+if __name__ == '__main__':
+    seed_all()
