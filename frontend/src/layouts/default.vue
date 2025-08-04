031f4a9adca0f079a866c30b5e7b8ebdf745a3a5
Le Minh Triet <trietlm0306@gmail.com>
2025-08-04 15:13:11 +0700

    [Feature] Add frontend framework by Vue.js and Vuetify

---
+++ frontend/src/layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+import { VApp } from 'vuetify/components';
+</script>
+
+<template>
+    <v-app>
+        <v-app-bar title="ITAPIA - Intelligent and Transparent AI-Powered Personal Investment Assistant" elevation="2"></v-app-bar>
+        <v-main>
+            <RouterView />
+        </v-main>
+        <v-footer app>
+            <span>&copy; {{ new Date().getFullYear() }} - ITAPIA Project</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+    import { ref } from 'vue';
+
+    const tickerSym = ref('')
+
+    function startAnalysis() {
+        if (tickerSym.value) {
+            alert("Starting analyze ....")
+        } else {
+            alert("Please input an valid symbol")
+        }
+    }
+</script>
+
+<template>
+    <v-container class="fill-height">
+        <v-row justify="center" align="center">
+            <v-col cols="12" md="8" lg="6">
+                <v-card elevation="4" class="pa-4">
+                    <VCardTitle class="text-center"></VCardTitle>
+
+                    <VCardText>
+                        <VTextField
+                            label="Input Ticker symbol (ex. AAPL)"
+                            variant="outlined"
+                            v-model="tickerSym"
+                        >
+                        </VTextField>
+
+                        <VBtn block color="primary" size="large" :onclick="startAnalysis">Analysis</VBtn>
+                    </VCardText>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
